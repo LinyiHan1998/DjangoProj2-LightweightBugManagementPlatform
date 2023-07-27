c57062c3d62e724535322d99f4b2b95fe64dca92
LinyiHan1998 <linyihan25@gmail.com>
2023-07-26 22:50:16 -0700

    Middleware completed: hightlight the nav-bar item that user is currently visiting

--- web/templatetags/project.py
+++ web/templatetags/project.py
@@ -1,5 +1,6 @@
 from django.template import Library
 from web import models
+from django.urls import reverse
 
 register = Library()
 
@@ -10,3 +11,18 @@ def all_project_list(request):
     #2.获取参与的所有项目
     join_proj_list = models.ProjectUser.objects.filter(userId=request.tracer.user)
     return {'my':create_proj_list,'join':join_proj_list}
+
+@register.inclusion_tag('inclusion/manage_menu_list.html')
+def manage_menu_list(request):
+    data_list = [
+        {'title':'Dashboard','url':reverse("dashboard",kwargs={'project_id':request.tracer.project.id})},
+        {'title': 'Issue', 'url': reverse("issue", kwargs={'project_id': request.tracer.project.id})},
+        {'title': 'Statistics', 'url': reverse("statistics", kwargs={'project_id': request.tracer.project.id})},
+        {'title': 'wiki', 'url': reverse("wiki", kwargs={'project_id': request.tracer.project.id})},
+        {'title': 'File', 'url': reverse("file", kwargs={'project_id': request.tracer.project.id})},
+        {'title': 'Setting', 'url': reverse("setting", kwargs={'project_id': request.tracer.project.id})},
+    ]
+    for item in data_list:
+        if request.path_info.startswith(item['url']):
+            item['class']='active'
+    return {'data_list':data_list}
